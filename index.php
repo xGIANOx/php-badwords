f6f9aaca3fb7ecb3fd39f45fdd2771b4b23d4309
xGIANOx <studioenricogian@gmail.com>
2023-05-03 15:20:36 +0200

    [add] form

--- index.php
+++ index.php
@@ -5,8 +5,30 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP-badwords</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 <body>
 
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <form action="myscript.php" method="get">
+                    <div class="my-5">
+                        <label for="name">Name</label>
+                        <input type="text" name="name" id="name" class="form-control" placeholder="Insert Name" aria-descridedby="suffixid">
+                    </div>
+                    <div class="my-5">
+                        <label for="badword">Badword</label>
+                        <input type="text" name="badword" id="badword" class="form-control" placeholder="Insert Badword" aria-descridedby="suffixid">
+                    </div>
+
+                    <button type="submit" class="btn btn-success">Submit</button>
+                    <button type="reset" class="btn btn-warning">Reset</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 </html>
